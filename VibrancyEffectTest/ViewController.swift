3d4b86a2791580910452293aa3bda067d90fb41f
Warif Akhand Rishi <rishi420@gmail.com>
2016-02-21 02:58:37 +0600

    Sample project added

---
+++ VibrancyEffectTest/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  VibrancyEffectTest
+//
+//  Created by Warif Akhand Rishi on 2/21/16.
+//  Copyright © 2016 Warif Akhand Rishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func playTextButtonAction(sender: AnyObject) {
+        print("playTextButtonAction")
+    }
+    
+    @IBAction func playImageButtonAction(sender: AnyObject) {
+        print("playImageButtonAction")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
